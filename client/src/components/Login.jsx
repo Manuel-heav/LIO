269e5a8528e483549f664aa3f89ff166f3055fc0
manuel-heav <manuelheav32@gmail.com>
2022-05-07 20:21:48 +0300

    working on login Page

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className="login">
+        <div className="login__container">
+            <form>
+                <label>Username</label>
+                <input type="text" name="username" />
+                <label>Password</label>
+                <input type="text" name="" id="" />
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
--- client/src/index.js
+++ client/src/index.js
@@ -6,18 +6,18 @@ import {
   Route
 } from "react-router-dom";
 import App from './App';
+import Login from './components/Login';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <BrowserRouter>
   <Routes>
     <Route path="/" element={<App />}/>
+    <Route path="/login" element={<Login />}/>
   </Routes>
   </BrowserRouter>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
