6de2a2c17535e895c90077655494dbc87db2f8ae
manuel-heav <manuelheav32@gmail.com>
2022-05-08 21:09:35 +0300

    working on header

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,8 +3,17 @@ import './login.css'
 const Login = () => {
   return (
     <div className="login">
+      <div className="login__header">
+        <h1>LIO</h1>
+        <div className="login__btn__container">
+          <button>Login</button>
+          <button>Sign up for free</button>
+        </div>
+      </div>
+      <div className="main__container">
         <div className="login__container">
-            <h1>LIO</h1>
+            <h1>Sign In for LIO</h1>
+            <h2>Write what you feel.</h2>
             <form>
                 <label>Username</label>
                 <input type="text" name="username" placeholder="Enter Username..." />
@@ -12,6 +21,7 @@ const Login = () => {
                 <input type="password" name="password" placeholder="Enter Password..." />
                 <button>Log In</button>
             </form>
+        </div>
         </div>
     </div>
   )
