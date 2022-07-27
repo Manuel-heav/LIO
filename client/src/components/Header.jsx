665070fdae5de9bb193395ffe2caf089b01d72b1
manuel-heav <manuelheav32@gmail.com>
2022-07-27 07:42:41 -0700

    working on header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import './header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 const Header = () => {
   return (
     <div className="header">
-        <h1>Lio</h1>
+        <div className="header__wrapper">
+          <h1>Lio</h1>
+          <div className="header__search__container">
+            <SearchIcon fontSize='small'/>
+            <input type="text" />
+          </div>
+          <AccountCircleIcon />
+        </div>     
     </div>
   )
 }
