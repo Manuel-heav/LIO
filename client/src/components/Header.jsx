28adedad547b47d0b29bd0fbd547ec66bb791939
manuel-heav <manuelheav32@gmail.com>
2022-07-28 08:45:15 -0700

    done with header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,12 +6,17 @@ const Header = () => {
   return (
     <div className="header">
         <div className="header__wrapper">
-          <h1>Lio</h1>
-          <div className="header__search__container">
-            <input type="text" placeholder="Search..." />
-            <SearchIcon fontSize='small'/>
+          <div className="header__left__wrapper">
+            <h1>Lio</h1>
+            <div className="header__search__container">
+              <input type="text" placeholder="Search..." />
+              <SearchIcon fontSize='small'/>
+            </div>
           </div>
-          <AccountCircleIcon />
+          <div className="header__account__container">
+           <AccountCircleIcon fontSize='large' />
+            <p>Login</p>
+          </div>
         </div>     
     </div>
   )
