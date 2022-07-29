2b909d0803be08d0571160ce30bfdc43b29f575a
manuel-heav <manuelheav32@gmail.com>
2022-07-29 06:55:34 -0700

    working on center

--- client/src/components/TopHosts.jsx
+++ client/src/components/TopHosts.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import "./tophosts.css"
 const TopHosts = () => {
   return (
     <div className="tophosts">
-      <div className="tophost__item">
+            <div className="card__top__container">
+            <div className="card__top__left">
                 <AccountCircleIcon fontSize='large' className="card__account__icon" />
-                <p>Manuel Heav</p>
+                <p>Host @manuelheav</p>
             </div>
+            <p>few seconds ago</p>
+        </div>
     </div>
   )
 }
