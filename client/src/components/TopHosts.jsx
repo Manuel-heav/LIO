36088a02188f221046c156b80b6e0dcda03f4318
manuel-heav <manuelheav32@gmail.com>
2022-07-29 07:05:06 -0700

    working on center

--- client/src/components/TopHosts.jsx
+++ client/src/components/TopHosts.jsx
@@ -4,13 +4,59 @@ import "./tophosts.css"
 const TopHosts = () => {
   return (
     <div className="tophosts">
+      <h2>Top Hosts</h2>
             <div className="card__top__container">
             <div className="card__top__left">
                 <AccountCircleIcon fontSize='large' className="card__account__icon" />
-                <p>Host @manuelheav</p>
+                <p>Manuel Heav</p>
             </div>
-            <p>few seconds ago</p>
-        </div>
+            <button>Follow</button>
+            </div>
+            <div className="card__top__container">
+            <div className="card__top__left">
+                <AccountCircleIcon fontSize='large' className="card__account__icon" />
+                <p>Manuel Heav</p>
+            </div>
+            <button>Follow</button>
+            </div>
+            <div className="card__top__container">
+            <div className="card__top__left">
+                <AccountCircleIcon fontSize='large' className="card__account__icon" />
+                <p>Manuel Heav</p>
+            </div>
+            <button>Follow</button>
+            </div>
+            <div className="card__top__container">
+            <div className="card__top__left">
+                <AccountCircleIcon fontSize='large' className="card__account__icon" />
+                <p>Manuel Heav</p>
+            </div>
+            <button>Follow</button>
+            </div>
+            <div className="card__top__container">
+            <div className="card__top__left">
+                <AccountCircleIcon fontSize='large' className="card__account__icon" />
+                <p>Manuel Heav</p>
+            </div>
+            <button>Follow</button>
+            </div>
+            <div className="card__top__container">
+            <div className="card__top__left">
+                <AccountCircleIcon fontSize='large' className="card__account__icon" />
+                <p>Manuel Heav</p>
+            </div>
+            <button>Follow</button>
+            </div>
+            <div className="card__top__container">
+            <div className="card__top__left">
+                <AccountCircleIcon fontSize='large' className="card__account__icon" />
+                <p>Manuel Heav</p>
+            </div>
+            <button>Follow</button>
+            </div>
+
+            <p style={{margin: "auto", color:"#6164FF", textAlign: "center", cursor: "pointer"}}>More</p>
+
     </div>
   )
 }
