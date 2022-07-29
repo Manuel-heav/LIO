84641241aee77f2e380d9540957d095839c5b4a0
manuel-heav <manuelheav32@gmail.com>
2022-07-29 06:13:05 -0700

    working on center

--- client/src/components/Center.jsx
+++ client/src/components/Center.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import "./center.css"
 const Center = () => {
   return (
-    <div className="center">
-
+    <div className="center">  
+      <div className="center__top__wrapper">
+        <div className="left__wrapper">
+          <h2>ROOMS</h2>
+          <p>600 Rooms available</p>
+        </div>
+        <button>Create Room</button>
+      </div>
     </div>
   )
 }
