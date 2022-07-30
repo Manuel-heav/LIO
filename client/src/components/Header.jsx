6cd921e8bb77fd8aa09ef1955b4d577b51e274fb
manuel-heav <manuelheav32@gmail.com>
2022-07-30 01:37:58 -0700

    routing done :)

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import './header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div className="header">
         <div className="header__wrapper">
           <div className="header__left__wrapper">
-            <h1>Lio</h1>
+          <Link to="/"><h1 style={{color: "white"}}>Lio</h1></Link>  
             <div className="header__search__container">
               <input type="text" placeholder="Search..." />
               <SearchIcon fontSize='small'/>
@@ -17,8 +18,8 @@ const Header = () => {
             <h2>Contact</h2>
           </div>
           <div className="header__account__container">
-            <button>Join</button>
-            <p>Login</p>
+           <Link to="signup"><button>Join</button></Link>
+            <Link to="login"><p>Login</p></Link>
           </div>
         </div>     
     </div>
