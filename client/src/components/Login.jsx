c2ed4efe08dd5c6dfb2ef899e4322a02a2183377
manuel-heav <manuelheav32@gmail.com>
2022-08-01 00:33:45 -0700

    working on login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-
+import "./login.css"
 const Login = () => {
   return (
     <div className="login">
+      <div className="login__container">
       <h1>Lio</h1>
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Passsword" />
       <button>Login</button>
       <p>Don't have an account? Sign up</p>
+      </div>
     </div>
   )
 }
