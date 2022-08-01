edc9550962f598c954edcc16475c694c0f100017
manuel-heav <manuelheav32@gmail.com>
2022-08-01 00:25:33 -0700

    working on login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Login = () => {
   return (
     <div className="login">
-
+      <h1>Lio</h1>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Passsword" />
+      <button>Login</button>
+      <p>Don't have an account? Sign up</p>
     </div>
   )
 }
