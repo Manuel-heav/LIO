3d828f52df49ea3f01015f60fcc37e190b2fb24a
manuel-heav <manuelheav32@gmail.com>
2022-08-20 17:52:22 +0300

    new commit

--- client/src/components/Center.jsx
+++ client/src/components/Center.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Card from './Card'
-import "./center.css"
+import React from 'react';
+import Card from './Card';
+import "./center.css";
 const Center = () => {
   return (
     <div className="center">  
