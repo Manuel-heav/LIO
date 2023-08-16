bdfa6ba2628f268e58268371df51c89c55d51fa4
manuel-heav <manuelheav32@gmail.com>
2023-08-16 20:16:59 +0300

    working on backend

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,9 +1,11 @@
-"use client"
+"use client";
 
+import * as z from "zod";
+import Image from "next/image";
 import { useForm } from "react-hook-form";
-
-import { Button } from "@/components/ui/button"
-import { Textarea } from "@/components/ui/textarea"
+import { usePathname, useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import {
   Form,
@@ -13,72 +15,48 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
-import { BsPersonFill } from "react-icons/bs";
-
-import { Input } from "@/components/ui/input"
-
-import { zodResolver } from "@hookform/resolvers/zod"
-import { userValidation } from "@/lib/validations/user";
-
-import * as z from "zod"
-import Image from "next/image";
-import { ChangeEvent, useState } from "react";
+import { useUploadThing } from "@/lib/uploadthing";
 import { isBase64Image } from "@/lib/utils";
 
-import { useUploadThing } from '@/lib/uploadthing'
+import { UserValidation } from "@/lib/validations/user";
+import { updateUser } from "@/lib/actions/user.actions";
+import { BsPersonFill } from "react-icons/bs";
 
-interface Props{
-    user: {
-        id: string,
-        objectId: string,
-        username: string,
-        name: string,
-        bio: string;
-        image: string;
-    };
-    btnTitle: string
+interface Props {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
 }
 
-const AccountProfile = ({user , btnTitle}) => {
-  const [files, setFiles] = useState<File[]>([])
+const AccountProfile = ({ user, btnTitle }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const { startUpload } = useUploadThing("media");
 
-  const { startUpload } = useUploadThing("media")
-   const form = useForm({
-    resolver: zodResolver(userValidation),
-    defaultValues:{
-        profile_photo: user?.image || "",
-        username: user?.username || "",
-        bio: user?.bio || ""
-    }
-   })
+  const [files, setFiles] = useState<File[]>([]);
 
-   function handleImage(e: ChangeEvent<HTMLInputElement>, fieldChange: (value:string) => void){
-    e.preventDefault();
+  const form = useForm<z.infer<typeof UserValidation>>({
+    resolver: zodResolver(UserValidation),
+    defaultValues: {
+      profile_photo: user?.image ? user.image : "",
+      name: user?.name ? user.name : "",
+      username: user?.username ? user.username : "",
+      bio: user?.bio ? user.bio : "",
+    },
+  });
 
-    const fileReader = new FileReader();
-
-    if(e.target.files && e.target.files.length > 0){
-      const file = e.target.files[0];
-      
-
-      setFiles(Array.from(e.target.files));
-
-      if(!file.type.includes('image')) return;
-
-
-      fileReader.onload = async (e) => {
-        const imageDataUrl = e.target?.result?.toString() || ''
-
-        fieldChange(imageDataUrl) 
-      }
-
-      fileReader.readAsDataURL(file)
-    }
-   }
-
-   const onSubmit = async (values: z.infer<typeof userValidation>) => {
+  const onSubmit = async (values: z.infer<typeof UserValidation>) => {
     const blob = values.profile_photo;
 
     const hasImageChanged = isBase64Image(blob);
@@ -89,38 +67,80 @@ const AccountProfile = ({user , btnTitle}) => {
         values.profile_photo = imgRes[0].fileUrl;
       }
     }
-  }
-    return(
-        <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col justify-start gap-10">
-          <FormField 
-            control={form.control}
-            name="profile_photo"
-            render={({ field }) => (
-              <FormItem className="flex items-center gap-4">
-                <FormLabel className="flex h-24 w-24 items-center justify-center rounded-full">
-                    {field.value ? (
-                        <Image src={field.value} alt="profile photo" width={96} height={96} priority className=" object-contain w-11 h-11 shrink-0 grow-0 rounded-full"/>
-                    ): (
-                        < BsPersonFill className="text-6xl p-1 rounded-full border-solid border-2 border-sky-300"/>
-                    )}
-                </FormLabel>
-                <FormControl className="flex-1 text-black">
-                  <Input 
-                type="file"
-                accept="image/*"
-                placeholder="Upload a Profile Picture"
-                className="cursor-pointer border-none outline-none file:text-blue"
-                onChange={e => handleImage(e, field.onChange)}
-/>
-                </FormControl>
-                <FormDescription className="block">
+
+    await updateUser({
+      name: values.name,
+      path: pathname,
+      username: values.username,
+      userId: user.id,
+      bio: values.bio,
+      image: values.profile_photo,
+    });
+
+    if (pathname === "/profile/edit") {
+      router.back();
+    } else {
+      router.push("/");
+    }
+  };
+
+  const handleImage = (
+    e: ChangeEvent<HTMLInputElement>,
+    fieldChange: (value: string) => void
+  ) => {
+    e.preventDefault();
+
+    const fileReader = new FileReader();
+
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+      setFiles(Array.from(e.target.files));
+
+      if (!file.type.includes("image")) return;
+
+      fileReader.onload = async (event) => {
+        const imageDataUrl = event.target?.result?.toString() || "";
+        fieldChange(imageDataUrl);
+      };
+
+      fileReader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        className='flex flex-col justify-start gap-10'
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
+          control={form.control}
+          name='profile_photo'
+          render={({ field }) => (
+            <FormItem className='flex items-center gap-4'>
+              <FormLabel className='account-form_image-label'>
+                {field.value ? (
+                  <Image src={field.value} alt="profile photo" width={96} height={96} priority className=" object-contain w-11 h-11 shrink-0 grow-0 rounded-full"/>
+                ) : (
+                  <BsPersonFill className="text-6xl p-1 rounded-full border-solid border-2 border-sky-300"/>
+                )}
+              </FormLabel>
+              <FormControl className='flex-1 text-base-semibold text-gray-200'>
+                <Input
+                  type='file'
+                  accept='image/*'
+                  placeholder='Add profile photo'
+                  className='cursor-pointer border-none outline-none file:text-blue'
+                  onChange={(e) => handleImage(e, field.onChange)}
+                />
+              </FormControl>
+              <FormDescription className="block">
                   Upload A Picture
                 </FormDescription>
                 <FormMessage />
-              </FormItem>
-            )}
-          />
+            </FormItem>
+          )}
+        />
 
 <FormField
             control={form.control}
@@ -172,9 +192,10 @@ const AccountProfile = ({user , btnTitle}) => {
             )}
           />
 
-          <Button type="submit" className="bg-sky-700">Submit</Button>
-        </form>
-      </Form>
-    )
-}
+        <Button type="submit" className="bg-sky-700">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
 export default AccountProfile;
